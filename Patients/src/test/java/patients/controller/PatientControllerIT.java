a3251390b945b0df91ff5991936799d18c1af1eb
Geoffrey Ouvrard <geoffrey.1796@gmail.com>
2022-01-16 22:15:37 +0100

    fixed the small bug on the phone number field of the patient infos update form (front-end)

--- Patients/src/test/java/patients/controller/PatientControllerIT.java
+++ Patients/src/test/java/patients/controller/PatientControllerIT.java
@@ -11,8 +11,6 @@ import org.springframework.test.web.servlet.MockMvc;
 
 import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.is;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
